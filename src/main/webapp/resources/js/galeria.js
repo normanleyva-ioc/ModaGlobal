452e1054e00f9f806ca738bb6f754dc35b909b9d
nlgarcia <nlgarcia@ioc>
2025-12-03 23:56:42 +0100

    Versió 1.1.1: Afegir galeria

--- src/main/webapp/resources/js/galeria.js
+++ src/main/webapp/resources/js/galeria.js
@@ -1,8 +1,13 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
 const imgElement = document.getElementById('imatge');
-imgElement.addEventListener('mouseenter', () => {
-  imgElement.style.transform = 'scale(1.1)';
+imgElement.addEventListener('mouseenter', ()=> {
+    imgElement.style.transform = 'scale(1.1)';
 });
 
 imgElement.addEventListener('mouseleave', ()=> {
     imgElement.style.transform = 'scale(1)';
-});
+})
